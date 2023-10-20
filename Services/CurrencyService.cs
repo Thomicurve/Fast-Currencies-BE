4623b047c55d695e90a77717854a947e82272c13
Thomas Curvelo Ferrer <tcurvelo@movizen.com>
2023-10-20 20:27:21 -0300

    Implementamos conversión de monedas

--- Services/CurrencyService.cs
+++ Services/CurrencyService.cs
@@ -4,17 +4,28 @@ public class CurrencyService
 {
     private readonly RequestService _requestService;
     private readonly ConvertionHistoryService _convertionHistoryService;
+    private readonly EntityRepository<Currency> _currencyRepository;
+
     public CurrencyService(
         RequestService requestService, 
-        ConvertionHistoryService convertionHistoryService)
+        ConvertionHistoryService convertionHistoryService,
+        EntityRepository<Currency> currencyRepository)
     {
         _requestService = requestService;
         _convertionHistoryService = convertionHistoryService;
+        _currencyRepository = currencyRepository;
     }
 
     public decimal Convert(ConvertCurrencyDto dto) {
         _requestService.IncrementRequestsCount();
-        _convertionHistoryService.RegisterHistory(dto.FromCurrencyId, dto.ToCurrencyId);
-        return dto.Amount * 2;
+        Currency? currencyFromConvert = _currencyRepository.GetById(dto.FromCurrencyId);
+        Currency? currencyToConvert = _currencyRepository.GetById(dto.ToCurrencyId);
+
+        if (currencyToConvert is null || currencyFromConvert is null)
+        {
+            throw new Exception("Currency not found");
+        }
+
+        return (dto.Amount * currencyFromConvert.IC) / currencyToConvert.IC;
     }
 }
